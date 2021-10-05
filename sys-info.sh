05d4b7014e23bdbbb4d7277ccb45610b61b65c29
Ricardo Mendes <mendes.r@gmx.com>
2021-10-05 21:14:41 +0100

    Finalize sys-info script

--- sys-info.sh
+++ sys-info.sh
@@ -25,24 +25,36 @@ report_disk_space () {
 }
 
 report_home_space () {
+	local format="%8s%10s%10s\n"
+	local i dir_list total_files total_dirs total_size user_name
+
 	if [[ "$(id -u)" -eq 0 ]]; then
+		dir_list=/home/*
+		user_name="All Users"
+
 		if [[ "$OSTYPE" == "darwin"* ]]; then #MacOS
-			cat <<- _EOF_
-				<h2>Home Space Utilization (All Users MacOS)</h2>
-				<pre>$(du -sh /Users/*)</pre>
-				_EOF_
-		else
-			cat <<- _EOF_
-				<h2>Home Space Utilization (All Users)</h2>
-				<pre>$(du -sh /home/*)</pre>
-				_EOF_
+			dir_list=/Users/*
 		fi
 	else
-		cat <<- _EOF_
-			<h2>Home Space Utilization ($USER)</h2>
-			<pre>$(du -sh $HOME)</pre>
-			_EOF_
+		dir_list="$HOME"
+		user_name="$USER"
 	fi
+
+	echo "<h2>Home Space Utilization ($user_name)</h2>"
+
+	for i in $dir_list; do 
+
+		total_files="$(find "$i" -type f | wc -l)"
+		total_dirs="$(find "$i" -type d | wc -l)"
+		total_size="$(du -sh "$i" | cut -f 1)"
+
+		echo "<h3>$i<h3>"
+		echo "<pre>"
+		printf "$format" "Dirs" "Files" "Size"
+		printf "$format" "----" "-----" "----"
+		printf "$format" "$total_dirs" "$total_files" "$total_size"
+		echo "</pre>"
+	done
 	return
 }
 
@@ -76,16 +88,16 @@ filename=
 while [[ -n "$1" ]]; do
 	case "$1" in
 		-f | --file)		 shift
-							filename="$1"
-							;;
+					filename="$1"
+					;;
 		-i | --interactive) interactive=1
-							;;
-		-h | --help			usage
-							exit
-							;;
-		*)					usage >&2
-							exit 1
-							;;
+					;;
+		-h | --help)		usage
+					exit
+					;;
+		*)			usage >&2
+					exit 1
+					;;
 	esac
 	shift
 done
@@ -120,7 +132,7 @@ if [[ -n "$filename" ]]; then
 	if touch "$filename" && [[ -f "$filename" ]]; then
 		write_html_page > "$filename"
 	else
-		echo "#PROGNAME: Cannit write file '$filename'" >&2
+		echo "$PROGNAME: Cannot write file '$filename'" >&2
 		exit 1
 	fi
 else
