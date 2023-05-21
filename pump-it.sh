2ff5452c33952fc080bdac6618ec1c2ec911314d
Ricardo Mendes <mendes.r@gmx.com>
2023-05-21 11:36:54 +0100

    Refactor repo and define a main script

---
+++ pump-it.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+OS
+PKG
+SLIM=""
+FAT=""
+
+function get-os() {
+	OS=$(cat /etc/os-release | grep '^ID=*' | cut -d = -f 2 | tr a-z A-Z)  
+}
+
+function get-pkg() {
+	case $OS in
+		FEDORA) PKG=dnf;;
+		UBUNTU) PKG=apt-get;;
+		*)PKG=error;;
+	esac
+}
+
+function spinout() {
+	PID=$1
+	i=1
+	sp="/-\|"
+	echo -n ' '
+	while [ -d /proc/$PID ]; do
+  		printf "\b${sp:i++%${#sp}:1}"
+		sleep 0.2
+	done
+}
+
+function install() {
+	for program in $PRG; do
+		echo "start $program instalation ..."
+		$PKG $program -y &
+		spinout $!
+	done
+}
+
+clear
+
+get-os
+echo "Let's personalize this $OS distro"
+get-pkg
+echo "We will be using the $PKG package manager"
+
+while true; do
+	read -p "SLIM or FAT installation? [Ss/Ff] " -n 1 answer
+	case $answer in
+		[Ss]*) echo -e "\nSLIM installation"; break;;
+		[Ff]*) echo -e "\nFAT installation"; break;;	
+		*) echo "";;
+	esac
+done
+
+# $PKG update -y && $PKG upgrade -y
+
+sleep 5 &
+spinout $!
+
+clear
