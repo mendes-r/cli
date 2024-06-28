c24b7f25cca75954a3dc5810864d430a7cbade40
Ricardo Mendes <mendes.r@gmx.com>
2024-06-28 21:59:37 +0100

    ADD sysstat to base script

--- scripts_tree/base/base_script.sh
+++ scripts_tree/base/base_script.sh
@@ -44,7 +44,7 @@ parse_params() {
 setup_colors
 parse_params "$@"
 
-PROGRAMS="git tree curl wget nmap openssh-server"
+PROGRAMS="git tree curl wget nmap openssh-server sysstat"
 PKG=$1
 HOME_USER=$2
 
